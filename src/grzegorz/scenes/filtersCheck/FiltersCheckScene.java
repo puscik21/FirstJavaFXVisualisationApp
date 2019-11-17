f56c59c8912279d9b050ff73025ce0dd36721e97
grzesiekpuscizna <grzesiekpuscizna@gmail.com>
2019-11-17 19:38:53 +0100

    Added generic number of qubits in FiltersCheckScene

--- src/grzegorz/scenes/filtersCheck/FiltersCheckScene.java
+++ src/grzegorz/scenes/filtersCheck/FiltersCheckScene.java
@@ -4,8 +4,10 @@ import grzegorz.QBitState;
 import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import java.util.ArrayList;
@@ -14,6 +16,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class FiltersCheckScene {
+    @FXML
+    private AnchorPane comparePane;
 
     @FXML
     private VBox filtersVBox;
@@ -24,83 +28,37 @@ public class FiltersCheckScene {
     @FXML
     private VBox ticksVBox;
 
+    @FXML
+    private Label sceneTitle;
+
     private ArrayList<Image> filterImages;
     private ArrayList<Image> photonImages;
     private int[] aliceFilters;
     private QBitState[] bobQBitStates;
 
-    // TODO: 12.11.2019 values on the right
-    // TODO: 12.11.2019 some title or any necessary information
+    private double sizeScale = 1.0;
+    private double timeScale = 4.0; // TODO: 17.11.2019 search optimal val (default 4.0)
+
+    private final String TICK_ICON_PATH = "grzegorz\\images\\tickIcon.png";
+    private final String ONE_ICON_PATH = "grzegorz\\images\\oneIcon.png";
+    private final String ZERO_ICON_PATH = "grzegorz\\images\\zeroIcon.png";
+
+
     // TODO: 16.11.2019 comments on right click
+    // TODO: 17.11.2019 scene title
 
     public void start(int[] aliceFilters, QBitState[] bobQBitStates) {
         this.aliceFilters = aliceFilters;
         this.bobQBitStates = bobQBitStates;
-        prepareQBitsAndFilters();
 
-        int imagesToView = aliceFilters.length;
-        addImageViews(imagesToView);
-        double scaleValue = getScaleValue();
-        System.out.println(ticksVBox.getScaleX());
-        scaleNodes(scaleValue, filtersVBox, ticksVBox, qBitsVBox);      /// scale AnchorPane? but w8 for numbers
-        System.out.println(ticksVBox.getScaleX());
-
-
-        prepareQBitsAndFilters();
+        prepareScene();
         scheduleAnimationStart();
     }
 
-    private void addImageViews(int quantity) {
-        for (int i = 0; i < quantity; i++) {
-            filtersVBox.getChildren().add(new ImageView(filterImages.get(0)));
-            ticksVBox.getChildren().add(new ImageView("grzegorz\\images\\tickIcon.png"));
-////            ticksVBox.getChildren().add(new ImageView("grzegorz\\images\\oneIcon.png"));
-//            ticksVBox.getChildren().add(new ImageView("grzegorz\\images\\zeroIcon.png"));
-            qBitsVBox.getChildren().add(new ImageView(photonImages.get(0)));
-        }
-    }
-
-    private double getScaleValue() {
-        int size = qBitsVBox.getChildren().size();
-//        double scaleFor7 = 0.35;
-        double scaleFor7 = 1;
-        if (size == 0) {
-            return scaleFor7;
-        }
-
-        double multiplier = 7.0 / size;
-        double scaleValue = multiplier * scaleFor7;
-
-        if (scaleValue < 0.3) {
-            scaleValue = 0.3;
-        } else if (scaleValue > 1.75) {
-            scaleValue = 1.75;
-        }
-        return scaleValue;
-//        return 0.6;
-    }
-
-    private void scaleNodes(double scaleValue, Node... nodes) {
-        for (Node node : nodes) {
-            node.setScaleX(scaleValue);
-            node.setScaleY(scaleValue);
-        }
-    }
-
-    private void scheduleAnimationStart() {
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                prepareWholeAnimation().play();
-            }
-        };
-        Timer timer = new Timer();
-        long delay = 1500L;
-        timer.schedule(task, delay);
-    }
-
-    private void prepareQBitsAndFilters() {
+    private void prepareScene() {
         prepareImages();
+        addImageViews();
+        scaleComparePane();
         prepareQBitsHBox();
         prepareFiltersHBox();
     }
@@ -119,6 +77,51 @@ public class FiltersCheckScene {
         filterImages.addAll(Arrays.asList(whiteFilter, greenFilter));
     }
 
+    private void addImageViews() {
+        int quantity = aliceFilters.length;
+        for (int i = 0; i < quantity; i++) {
+            filtersVBox.getChildren().add(new ImageView(filterImages.get(0)));
+            qBitsVBox.getChildren().add(new ImageView(photonImages.get(0)));
+            ImageView invisibleTick = new ImageView(TICK_ICON_PATH);
+            invisibleTick.setVisible(false);
+            ticksVBox.getChildren().add(invisibleTick);
+        }
+    }
+
+    private void prepareScaleValues() {
+        int size = qBitsVBox.getChildren().size();
+        if (size == 0) {
+            return;
+        }
+
+        setSizeScale(size);
+        setTimeScale(size);
+    }
+
+    private void setSizeScale(double size) {
+        double multiplier = 7.0 / size;
+        sizeScale *= multiplier;
+
+        if (sizeScale < 0.3) {
+            sizeScale = 0.3;
+        } else if (sizeScale > 1.75) {
+            sizeScale = 1.75;
+        }
+    }
+
+    private void setTimeScale(double size) {
+        timeScale = timeScale / size;
+        if (timeScale > 1.0) {
+            timeScale = 1.0;
+        }
+    }
+
+    private void scaleComparePane() {
+        prepareScaleValues();
+        comparePane.setScaleX(sizeScale);
+        comparePane.setScaleY(sizeScale);
+    }
+
     private void prepareQBitsHBox() {
         for (int i = 0; i < qBitsVBox.getChildren().size(); i++) {
             int imageNumber = bobQBitStates[i].getState();
@@ -135,27 +138,70 @@ public class FiltersCheckScene {
         }
     }
 
+    private void scheduleAnimationStart() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                prepareWholeAnimation().play();
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 1500L;
+        timer.schedule(task, delay);
+    }
+
     private SequentialTransition prepareWholeAnimation() {
         int size = ticksVBox.getChildren().size();
         ArrayList<Animation> transitions = new ArrayList<>();
+        ArrayList<Integer> indexesOfCorrect = new ArrayList<>(size);
 
         for (int i = 0; i < size; i++) {
             if (!bobQBitStates[i].isFilterProper(aliceFilters[i])) {
                 continue;
             }
+            indexesOfCorrect.add(i);
             Node node = ticksVBox.getChildren().get(i);
             node.setVisible(true);
-            transitions.add(getFadeTransition(node, 0.0, 1.0));
+            transitions.add(getFadeTransition(node, 0.0, 1.0, 2 * timeScale));
         }
         Animation[] transArray = new Animation[transitions.size()];
         transArray = transitions.toArray(transArray);
-        return new SequentialTransition(transArray);
+        SequentialTransition showTicksTransition = new SequentialTransition(transArray);
+        SequentialTransition showNumbersTransition = getTicksToNumbersTransition(indexesOfCorrect);
+
+        return new SequentialTransition(showTicksTransition, showNumbersTransition);
     }
 
-    private FadeTransition getFadeTransition(Node node, double fromVal, double toVal) {
+    private SequentialTransition getTicksToNumbersTransition(ArrayList<Integer> indexes) {
+        Animation[] transitions = new Animation[indexes.size()];
+        int counter = 0;
+        for (int i : indexes) {
+            int qBitVal = bobQBitStates[i].getValue();
+            ImageView node = (ImageView) ticksVBox.getChildren().get(i);
+            SequentialTransition transition = getChangeNumberTransition(node, qBitVal);
+            transitions[counter] = transition;
+            counter++;
+        }
+        return new SequentialTransition(transitions);
+    }
+
+    private SequentialTransition getChangeNumberTransition(ImageView node, int qBitVal) {
+        FadeTransition hideTransition = getFadeTransition(node, 1.0, 0.0, timeScale);
+        hideTransition.setOnFinished(e -> changeNumberImage(node, qBitVal));
+        FadeTransition showTransition = getFadeTransition(node, 0.0, 1.0, timeScale);
+
+        return new SequentialTransition(hideTransition, showTransition);
+    }
+
+    private void changeNumberImage(ImageView node, int qBitVal) {
+        String path = qBitVal == 1 ? ONE_ICON_PATH : ZERO_ICON_PATH;
+        node.setImage(new Image(path));
+    }
+
+    private FadeTransition getFadeTransition(Node node, double fromVal, double toVal, double time) {
         FadeTransition fadeTransition = new FadeTransition();
         fadeTransition.setNode(node);
-        fadeTransition.setDuration(Duration.seconds(1));
+        fadeTransition.setDuration(Duration.seconds(time));
         fadeTransition.setFromValue(fromVal);
         fadeTransition.setToValue(toVal);
         return fadeTransition;
